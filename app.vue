1454f2f66bf8ea65b4c794a8eca7e4572dfef7d3
Thammapakorn <121923286+thammapakorn@users.noreply.github.com>
2025-01-26 00:05:28 +0700

    ทำการลบ app.vue และให้แสดงหน้า index.vue แทน

--- app.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div>
-    <h1>Pream</h1>
-  </div>
-</template>
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <h1>หน้าแรก index.vue</h1>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped></style>
