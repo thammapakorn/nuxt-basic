ab52d6e186a827758197d419e08582fc632403cf
Thammapakorn <121923286+thammapakorn@users.noreply.github.com>
2025-01-26 11:41:18 +0700

    สร้าง NuxtLink

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,20 @@
 <template>
     <div>
+        <header>
+            <nav>
+                <h1>Resume</h1>
+                <NuxtLink to="/">หน้าแรก</NuxtLink>
+                <NuxtLink to="/about">เกี่ยวกับเรา</NuxtLink>
+                <NuxtLink to="products">สินค้าทั้งหมด</NuxtLink>
+            </nav>
+        </header>
         <h1>หน้าแรก index.vue</h1>
     </div>
 </template>
 
 <script setup>
+import { defineNuxtLink } from 'nuxt/app';
+
 
 </script>
 
