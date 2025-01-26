02bbbc196795ab83e6d03c11d83b7973a1ae1f5d
Thammapakorn <121923286+thammapakorn@users.noreply.github.com>
2025-01-26 12:55:20 +0700

    วิธีใช้ layout และ slot
    สร้าง folder layouts และสร้างไฟล์ default.vue ใส่ header และ style ที่ต้องการ
    ส่วนเนื้อหาใช้ <slot/> เพื่อแสดงข้อมูลในหน้าเว็บของแต่ละหน้า

---
+++ layouts/default.vue
@@ -0,0 +1,45 @@
+<template>
+    <div>
+        <header>
+            <nav>
+                <h1 class="logo">Resume</h1>
+                <NuxtLink to="/">หน้าแรก</NuxtLink>
+                <NuxtLink to="/about">เกี่ยวกับเรา</NuxtLink>
+                <NuxtLink to="products">สินค้าทั้งหมด</NuxtLink>
+            </nav>
+        </header>
+        <div>
+            <slot/>
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+nav{
+    margin: 10px auto;
+    padding: 10px 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: #c58d8d;
+}
+nav a{
+    color: white;
+    text-decoration: none;
+    margin-left: 12px;
+    font-size: 18px;
+    padding: 10px;
+}
+nav a:hover{
+    color: aquamarine;
+}
+.logo{
+    margin-right: auto;
+    padding-left: 10px;
+    color: white;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,8 @@
 <template>
     <div>
         <header>
-            <nav>
-                <h1 class="logo">Resume</h1>
-                <NuxtLink to="/">หน้าแรก</NuxtLink>
-                <NuxtLink to="/about">เกี่ยวกับเรา</NuxtLink>
-                <NuxtLink to="products">สินค้าทั้งหมด</NuxtLink>
-            </nav>
+            <h1>หน้าแรก index.vue</h1>
         </header>
-        <h1>หน้าแรก index.vue</h1>
     </div>
 </template>
 
@@ -17,27 +11,5 @@
 </script>
 
 <style scoped>
-nav{
-    margin: 10px auto;
-    padding: 10px 0;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    background-color: #c58d8d;
-}
-nav a{
-    color: white;
-    text-decoration: none;
-    margin-left: 12px;
-    font-size: 18px;
-    padding: 10px;
-}
-nav a:hover{
-    color: aquamarine;
-}
-.logo{
-    margin-right: auto;
-    padding-left: 10px;
-    color: white;
-}
+
 </style>
